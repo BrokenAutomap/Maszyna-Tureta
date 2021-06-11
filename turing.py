c3e1c459eaaf3e91f2a0d9e90eef6acb469112a1
BrokenAutomap <sebastian.misiarz1@gmail.com>
2021-06-11 10:49:00 +0200

    basic turing machine emulator compiler for text code input started in test.py

--- turing.py
+++ turing.py
@@ -21,18 +21,16 @@ class AbstractTuringMachine(metaclass=ABCMeta):
     def action(self, state):
         pass
 # s0 = <,>,|; 1,3,4 ; 1,3,4:s1, 4,2,1: s2, 1,5,5:s2
-# State structure: [s0, [<, >, |, ...], [1, #, 4, _, ...], {[1, #, #, ...]: 's1', [#, #, #, ...]:'s2', ...}]
+# State structure: 's0' : {[1, #, #, ...]: ['s1', [L, R, N, ...], [1, #, 4, _, ...]],
+# [#, #, #, ...]:['s2',[<, >, |, ...], [1, #, 4, _, ...]], ...}
 # state[0] - state name
-# state[1] - the list of tape movements, if first sign is < then it moves the first tape to the left and so on
-# state[2] - the list of signs being placed at each tape, in the example above 4 is being placed on the third tape
-# the '_' means that nothing is changed and the '#' is a blank cell
-# state[3] - the dictionary of possible next states, if the read (focused) values correspond to one of the dictionary
-# keys, according state is chosen
+# state[1] - the dictionary of possible next states, if the read (focused) values correspond to one of the dictionary
+# keys, according state is chosen, additionally each of the transitions to a new state (or the same one) is met with
+# specific movement of the tape which is written in the dictionary value on the second element of the list
 
 
 class TuringMachine(AbstractTuringMachine):
-    def __init__(self, input, tapes_num):
-        super().__init__()
+    def __init__(self, input, tapes_num, **kwargs):
         self.starting_tape = input
         self.tapes = []
         self.focused_cells = []
@@ -40,22 +38,27 @@ class TuringMachine(AbstractTuringMachine):
             self.tapes.append(['#'])
             self.focused_cells.append(0)
             if tape_index == 0:
-                self.tapes[0] = input
-        self.state_list = []
+                self.tapes.extend(list(input))
+        self.state_dict = {}
         self.initial_state = 0
         self.final_states = []
+        self.file_name = ''
+        for key, value in kwargs:
+            if key == 'filename':
+                self.file_name = value
 
     def move_tape(self, order):
         tape_index = 0
         for move in order:
-            if move == '<':
+            if move == 'L':
                 if self.focused_cells[tape_index] == 0:
                     self.tapes[tape_index].insert(0, '#')
                 else:
                     self.focused_cells[tape_index] -= 1
-            elif move == '>':
+            elif move == 'R':
                 if self.focused_cells[tape_index] == len(self.tapes[tape_index])-1:
                     self.tapes[tape_index].append('#')
+
                 self.focused_cells[tape_index] += 1
             else:
                 pass
@@ -73,16 +76,19 @@ class TuringMachine(AbstractTuringMachine):
         if self.is_end(state):
             return 1
         else:
-            next_state = state[3].get(self.get_focused())
+            transition = state[1].get(self.get_focused())
+            if transition == None:
+                transition = state[1].get('default')
+            next_state = transition[0]
             tape_index = 0
-            for value in state[2]:
+            for value in transition[2]:
                 if value == '_':
                     pass
                 else:
-                    self.state_list[tape_index][2][tape_index] = value
+                    self.tapes[tape_index][self.focused_cells[tape_index]] = value
                 tape_index += 1
-            self.move_tape(state[1])
-            self.action(next_state)
+            self.move_tape(transition[1])
+            self.action(self.state_dict[next_state])
 
     def get_focused(self):
         focused_values = []
@@ -94,7 +100,34 @@ class TuringMachine(AbstractTuringMachine):
     def print_data(self):
         pass
 
-    def add_state(self, state):
-        self.state_list.append(state)
+    def add_state(self, state_name):
+        self.state_dict.update({state_name: {}})
 
+    def add_transition(self, state_name, transition):
+        self.state_dict.update({state_name: {transition}})
+
+    """def read_code(self):
+        if self.file_name:
+            file = open(self.file_name, 'r')
+            for line in file:
+                if line == "^initial state":
+                    line.replace(" ", "")
+                    equal_sign_list = line.split("=")
+                    self.initial_state = equal_sign_list[1]
+                if line == "^final state":
+                    line.replace(" ", "")
+                    equal_sign_list = line.split("=")
+                    self.final_states = equal_sign_list[1].split(",")
+                if line == "^[]"
+    """
+
+
+
+                    # s0 = <,>,|; 1,3,4 ; 1,3,4:s1, 4,2,1: s2, 1,5,5:s2
+
+
+
+turing = TuringMachine("124+12356j+131+123j", 3)
+turing.add_state("start")
+turing.add_transition("start", {"default":["move to end", ['R', 'N', 'N'], ['_', '_', '_']]})
 
