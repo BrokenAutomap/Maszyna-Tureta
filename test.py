31ef6d46cd2111c871af29217f1df112f9536fc4
BrokenAutomap <sebastian.misiarz1@gmail.com>
2021-06-12 00:17:12 +0200

    tested and corrected turing

--- test.py
+++ test.py
@@ -1,47 +1,25 @@
-def read_code(self, path):
-    code = open(path, "r")
-    line = "a"
-    line_index = 0
-    while line != '':
-        line = code.readline()
-        line_index += 1
-        if line == "^initial state":
-            pass
-        elif line == "^final state":
-            pass
-        elif line == "^#+":
-            pass
+without_reminder = []
+with_reminder = []
+for x in range(0,10):
+    for y in range(0,10):
+        if x+y>9:
+            with_reminder.append((x,y))
         else:
-            definition = line.split('=')
-            definition[0].strip()
-            if definition[0] == "^[a-zA-Z0-9_]+$":
-                self.state_list[len(self.state_list) - 1][0] = definition[0]
-            else:
-                error_message = "Invalid state name at line: {} (state names can consist only of " \
-                                "upper and lowercase letters, numbers and underscores".format(line_index)
-                raise NameError(error_message)
+            without_reminder.append((x,y))
 
-            definition_data = definition[1].split(";")
-            definition_data[0].replace(" ", "")
-            tape_movement_list = definition_data[0].split(",")
-            for value in tape_movement_list:
-                if value != "^[<>|]$":
-                    error_message = "Invalid tape movement indicator at line: {} " \
-                                    "(only <, >, or | allowed)".format(line_index)
-                    raise Exception(error_message)
-            self.state_list[len(self.state_list) - 1][1] = tape_movement_list
 
-            definition_data[1].replace(" ", "")
-            replacement_list = definition_data[1].split(",")
-            for value in replacement_list:
-                if value == "":
-                    error_message = "Invalid replacement character at line: {} " \
-                                    "(can't be a whitespace)".format(line_index)
-                    raise Exception(error_message)
-            self.state_list[len(self.state_list) - 1][2] = replacement_list
+f = open("code.txt", "r")
+for line in f:
+    if line.startswith("turing.add_transition") and "default" not in line:
+        first_div = line.split("[")
+        second_div = first_div[1].split("]")
+        for_change = second_div[0]
+        for_change = for_change.replace("'", "")
+        for_change = for_change.replace(",", "")
+        for_change = for_change.replace(" ", "")
+        for_change = "'" + for_change + "'"
 
-            definition_data[2].replace(" ", "")
-            state_transitions_list = definition_data[2].split(",")
-            for state_transition in state_transitions_list:
-                key_value = state_transition.split(":")
-                key_value[0]
+        the_rest = line.split(":")
+        line = first_div[0] + for_change + ":" + the_rest[1]
+    print(line, end="")
+f.close()
